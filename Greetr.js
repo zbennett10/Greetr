a9032290e7e96375e6b2af3eade36366c9734eb1
Zachary Bennett <zbennett10@hotmail.com>
2017-01-27 18:51:36 -0500

    initial commit

---
+++ Greetr.js
@@ -0,0 +1,7 @@
+// When given a first name, last name, and optional language, it generates formal/informal greetings
+//supports english/spanish languages
+//Reusable library/framework
+//Easy to type 'G$()' structure
+//supports jquery
+
+
