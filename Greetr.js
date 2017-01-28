403837a92773f37586759c0630eb2c0640477615
Zachary Bennett <zbennett10@hotmail.com>
2017-01-27 19:05:54 -0500

    initial object setup

--- Greetr.js
+++ Greetr.js
@@ -5,3 +5,24 @@
 //supports jquery
 
 
+//IIFE that creates a safe structure for Greetr library initialization
+(function (global, $) {
+    var Greetr = function(firstName, lastName, language) {
+
+        //return results of a different function constructor so that 'new' keyword is not needed
+        return new Greetr.init(firstName, lastName, language);
+    }
+
+    //holds Greetr methods/object in order to conserve memory space
+    Greetr.prototype = {};
+
+    Greetr.init = function(firstName, lastName, language) {
+        var self = this;
+
+        self.firstName = firstName || 'John';
+        self.lastName = lastName || 'Doe';
+        self.language = language || 'en';
+    }
+    
+
+}(window, jQuery));
